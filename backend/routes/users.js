e6aeefc5a0d0a7b3f1327b7a9c6eb4abb119023e
ayedaden <aden.ayed@edu.ece.fr>
2021-11-25 22:33:17 +0100

    Routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+let datas = [{
+  id_film: "Titanic",
+}];
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res) {
+  const { id } = req.params;
+  const user = _.find(users, ["id_film", id]);
+  res.status(200).json({ datas });
+});
+
+router.put('/', function (req, res) {
+  const { user } = req.body;
+  const id = _.uniqueId();
+  users.push({ user, id });
+  res.json({
+    datas: { datas, id }
+  });
+});
+
+router.post('/', function (req, res) {
+  const id = req.params;
+  const { user } = req.body;
+  const userToUpdate = _.find(users, ["id", id]);
+  userToUpdate.datas = user;
+  res.json({
+    message: 'Just updated'
+  });
+});
+
+router.delete('/:id_film', function (req, res) {
+  const { id } = req.params;
+  _.remove(users, ["id_film", id]);
+  userToUpdate.datas = user;
+  res.json({
+    message: 'Just removed'
+  });
 });
 
 module.exports = router;
