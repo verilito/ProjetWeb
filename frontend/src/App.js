56d88fd2e5d4d88a31e517422685e96273f0cb7d
ayedaden <aden.ayed@edu.ece.fr>
2021-11-27 20:48:41 +0100

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import InitialComponent from "./components/InitialComponent";
-//import Monprofil from "./components/Monprofil";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-//<InitialComponent />
+
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<InitialComponent />} />
-
-    </Routes>
-  </BrowserRouter>
+  <InitialComponent />
 );
 
 
